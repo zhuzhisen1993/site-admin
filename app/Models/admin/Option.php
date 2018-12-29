dcef6b443e438d88aa2907bd1dc5d566a1c104a7
zzs <596885319@qq.com>
2018-12-29 15:44:55 +0800

    更新列表类型接口

--- app/Models/admin/Option.php
+++ app/Models/admin/Option.php
@@ -3,10 +3,16 @@
 namespace App\Models\admin;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\admin\OptionCatalog;
 
 class Option extends Model
 {
     protected $fillable = [
          'option_catalog_id','title'
     ];
+
+    public function optionCatalog(){
+        return $this->belongsTo(OptionCatalog::class);
+    }
+
 }
